856a12f0f260069b43d989661db1e40cb21b4dcb
Lincoln doVale <lincolndovale07@gmail.com>
2021-08-02 22:52:15 -0300

    Atualizacao do projeto no eclipse, estruturas de repeticao, Scanner

---
+++ br/edu/tp1/src/br/edu/tp1/testes/PrimeiroTeste.java
@@ -0,0 +1,67 @@
+package br.edu.tp1.testes;
+
+public class PrimeiroTeste {
+
+	private static String nome;
+	private static String sobrenome;
+	private static int idade;
+	private static float salario;
+	private static boolean java;
+	private static int qtdeMes;
+	
+	private static boolean validar(int qtde){
+		return qtde == 6;
+	}
+	
+	private static int calcularAnoNascimento(int idade){
+		return 2021 - idade;
+	}	
+	
+	private static float calcularSalarioTotal(float salario, int qtdeMes){
+		return salario * qtdeMes;
+	}
+	
+	private static String definirSituacao(int idade){		
+		return idade < 50 ? "Iniciante" : "Veterano";
+	}
+	
+	private static String definirStatus(float salarioTotal){		
+		return salarioTotal > 1000 ? "Estavel" : "Instavel";
+	}
+	
+	private static void tratarParametros(String[] argumentos){
+		nome = argumentos[0];
+		sobrenome = argumentos[1];
+		idade = Integer.valueOf(argumentos[2]);
+		salario = Float.valueOf(argumentos[3]);
+		java = Boolean.valueOf(argumentos[4]);
+		qtdeMes = Integer.valueOf(argumentos[5]);
+	}
+	
+	private static void impressao(){
+		float salarioTotal = calcularSalarioTotal(salario, qtdeMes);
+		
+		System.out.println("Nome: " + nome);
+		System.out.println("Sobrenome: " + sobrenome);
+		System.out.println("Idade: " + idade);
+		System.out.println("Ano de nascimento: " + calcularAnoNascimento(idade));
+		System.out.println("Salario mensal: " + salario);
+		System.out.println("Recebimentos: " + salarioTotal);
+		System.out.println("Devjava: " + java);
+		System.out.println("Situacao: " + definirSituacao(idade));
+		System.out.println("Status: " + definirStatus(calcularSalarioTotal(salario, qtdeMes)));
+	}
+
+	public static void main(String[] args){	
+		System.out.println("Hello, World");
+
+		if(validar(args.length)){
+			
+			tratarParametros(args);
+			
+			impressao();
+		} else {
+			System.out.println("Deu ruim");
+		}
+	}
+}
---
+++ br/edu/tp1/src/br/edu/tp1/testes/SegundoTeste.java
@@ -0,0 +1,49 @@
+package br.edu.tp1.testes;
+
+import java.util.Scanner;
+
+public class SegundoTeste {
+	
+	public static void main(String[] args){
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Informe o seu nome: ");
+		String nome = in.next();
+		
+		System.out.println("Informe a sua idade: ");		
+		int idade = in.nextInt();
+		
+		System.out.println("Informe o seu salario: ");
+		float salario = in.nextFloat();
+		
+		int quantidade = Integer.valueOf(args[0]);
+		
+		if(quantidade % 2 == 0){		
+			System.out.println("Numero par: " + quantidade);
+		} else {		
+			System.out.println("Numero impar: " + quantidade);
+		}
+		
+		//FOR: 
+		for(int i = 0;i < quantidade;i++){
+			System.out.println("FOR: " + nome +" - "+ idade +" - "+ salario);
+		}
+		
+		//WHILE: 
+		int a = 0;
+		while(a < quantidade){
+			System.out.println("WHILE: " + nome +" - "+ idade +" - "+ salario);
+			a++;
+		}
+		
+		//DOWHILE: 
+		int b = 0;
+		do{
+			System.out.println("DOWHILE: " + nome +" - "+ idade +" - "+ salario);
+			b++;
+		}while(b < quantidade);
+		
+		in.close();
+	}
+}
